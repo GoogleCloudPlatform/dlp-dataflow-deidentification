0b937034847249e1425bd13404aa589dca296485
Theodore Siu <theosiu@google.com>
2019-02-05 09:44:53 -0500

    Add Apache License and clean up

--- src/test/java/com/google/swarm/tokenization/common/UtilTest.java
+++ src/test/java/com/google/swarm/tokenization/common/UtilTest.java
@@ -1,6 +1,23 @@
+/*
+ * Copyright 2018 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+
 package com.google.swarm.tokenization.common;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
+
 import com.google.api.services.bigquery.model.TableSchema;
 import com.google.privacy.dlp.v2.Value;
 import org.apache.beam.sdk.io.Compression;
@@ -109,12 +126,7 @@ public class UtilTest implements Serializable{
         assertEquals(Util.findEncryptionType(null, null, null, null),false);
     }
 
-    @Test
-    public void testGetStorage(){
-        
 
-
-    }
 
     @Test
         public void testGetReader() throws IOException {
