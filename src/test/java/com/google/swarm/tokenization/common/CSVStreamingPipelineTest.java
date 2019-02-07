894235193c33b8a81a0b35c9a0d0ad5a68057618
Theodore Siu <theosiu@google.com>
2019-02-07 12:29:03 -0500

    Change unittest to just CSVContentDoFn

--- src/test/java/com/google/swarm/tokenization/common/CSVStreamingPipelineTest.java
+++ src/test/java/com/google/swarm/tokenization/common/CSVStreamingPipelineTest.java
@@ -1,10 +1,11 @@
 package com.google.swarm.tokenization.common;
 
-import com.google.cloud.dlp.v2.DlpServiceClient;
 import com.google.privacy.dlp.v2.*;
+import org.apache.beam.sdk.io.range.OffsetRange;
 import org.apache.beam.sdk.options.ValueProvider;
 import org.apache.beam.sdk.testing.PAssert;
 import org.apache.beam.sdk.transforms.Create;
+import org.apache.beam.sdk.transforms.DoFn;
 import org.apache.beam.sdk.transforms.Keys;
 import org.apache.beam.sdk.transforms.ParDo;
 import org.apache.beam.sdk.values.KV;
@@ -14,23 +15,21 @@ import org.junit.Test;
 
 import org.apache.beam.sdk.testing.TestPipeline;
 import org.junit.rules.ExpectedException;
-import org.mockito.Mockito;
 
-import java.io.IOException;
-import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
 import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
 import static org.mockito.Mockito.any;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.times;
 import static org.junit.Assert.*;
 
 
 public class CSVStreamingPipelineTest {
-
+    /*
     @Rule
     public final transient TestPipeline pipeline = TestPipeline.create();
 
@@ -56,47 +55,77 @@ public class CSVStreamingPipelineTest {
                 ParDo.of(new CSVContentProcessorDoFn(ValueProvider.StaticValueProvider.of(100))));
 
         PCollection<String> outputKeys = outputTables.apply(Keys.create());
-        assertNotNull(outputKeys);
+        //assertNotNull(outputKeys);
         //PAssert.that(outputKeys).empty();
-        //PAssert.that(outputKeys).containsInAnyOrder("Test1_1");
+        PAssert.that(outputKeys).containsInAnyOrder("Test1_1");
         pipeline.run();
+
+    }*/
+
+    @Test
+    public void testCSVStreamingInitialRestriction(){
+        CSVContentProcessorDoFn csv =
+                new CSVContentProcessorDoFn(ValueProvider.StaticValueProvider.of(2));
+        String[] lines1 = {"line1", "line2", "line3", "line4"};
+        String[] lines2 = {"line1", "line2", "line3", "line4", "line5", "line6"};
+
+        KV<String, List<String>> input1 = KV.of("FileName", Arrays.asList(lines1));
+        KV<String, List<String>> input2 = KV.of("FileName", Arrays.asList(lines2));
+
+        OffsetRange rangeResult1 = csv.getInitialRestriction(input1);
+        assertEquals(rangeResult1.getFrom(),1);
+        assertEquals(rangeResult1.getTo(),3);
+
+
+        OffsetRange rangeResult2 = csv.getInitialRestriction(input2);
+        assertEquals(rangeResult2.getFrom(),1);
+        assertEquals(rangeResult2.getTo(),4);
+
 
     }
 
     @Test
-    public void testDLPTokenizationDoFn() throws SQLException {
-        String headerString = "Header1,Header2";
-        List<FieldId> headers = Arrays.stream(headerString.split(","))
-                .map(header -> FieldId.newBuilder().setName(header).build()).collect(Collectors.toList());
+    public void testSplitRestriction(){
+        CSVContentProcessorDoFn csv =
+                new CSVContentProcessorDoFn(ValueProvider.StaticValueProvider.of(2));
+        OffsetRange off = new OffsetRange(2,5);
+        DoFn.OutputReceiver out = mock(DoFn.OutputReceiver.class);
 
-        List<String> lines = new ArrayList<>();
-        lines.add("test1,test2");
-        lines.add("foo,bar");
-        Table batchData = Util.createDLPTable(headers, lines);
+        String[] lines2 = {"line1", "line2", "line3", "line4", "line5", "line6"};
+        KV<String, List<String>> input1 = KV.of("FileName", Arrays.asList(lines2));
 
-        DLPTokenizationDoFn mockDLP = mock(DLPTokenizationDoFn.class, Mockito.RETURNS_DEEP_STUBS);
-        DlpServiceClient mockServiceClient = mock(DlpServiceClient.class, Mockito.RETURNS_DEEP_STUBS);
-        //DeidentifyContentResponse mockResponse = mock(DeidentifyContentResponse.class);
+        csv.splitRestriction(input1, off, out);
+        verify(out, times(3)).output(any(OffsetRange.class));
 
-        when(mockDLP.createDLPServiceClient()).thenReturn(mockServiceClient);
-        when(mockServiceClient.deidentifyContent(any(DeidentifyContentRequest.class)))
-                .thenThrow(new InterruptedException("DLP Client has deidentified"));
-        //when(mockServiceClient.deidentifyContent(any(DeidentifyContentRequest.class)))
-        //        .thenReturn(mockResponse);
+    }
 
-        PCollection<KV<String, Table>> input =
-                pipeline.apply(Create.of(KV.of("File1_1", batchData)));
-
-        exception.expect(InterruptedException.class);
-        PCollection<Row> tokenizedData =
-                input.apply("Tokenize", ParDo.of(mockDLP));
-
-        pipeline.run();
-
-
+    @Test
+    public void testNewTracker(){
+        CSVContentProcessorDoFn csv =
+                new CSVContentProcessorDoFn(ValueProvider.StaticValueProvider.of(2));
+        OffsetRange off = new OffsetRange(2,5);
+        org.apache.beam.sdk.transforms.splittabledofn.OffsetRangeTracker offTrack = csv.newTracker(off);
+        assertEquals(offTrack.currentRestriction(),off);
 
 
     }
+
+    @Test
+    public void testCreateLineBatches(){
+        CSVContentProcessorDoFn csv =
+                new CSVContentProcessorDoFn(ValueProvider.StaticValueProvider.of(2));
+        long i = 2;
+        KV<Integer,Integer> result1 = csv.createStartEnd(6,2);
+        KV<Integer,Integer> result2 = csv.createStartEnd(6,1);
+        assertEquals(result1, KV.of(3,4));
+        assertEquals(result2, KV.of(1,2));
+
+
+    }
+
+
+
+
 
 
 }
