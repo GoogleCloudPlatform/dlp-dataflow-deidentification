841bd5e3d55d4b8d235f9394111a2c2876380b4e
Masud <masudhasan@google.com>
2019-11-30 12:46:59 -0500

    cloud build dir change for template creation

---
+++ src/main/java/com/google/swarm/tokenization/tool/DLPTemplateHelper.java
@@ -0,0 +1,90 @@
+/*
+ * Copyright 2019 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.google.swarm.tokenization.tool;
+
+import static java.nio.charset.StandardCharsets.UTF_8;
+
+import com.google.cloud.storage.Blob;
+import com.google.cloud.storage.BlobId;
+import com.google.cloud.storage.BlobInfo;
+import com.google.cloud.storage.Storage;
+import com.google.cloud.storage.StorageOptions;
+import com.google.common.io.Resources;
+import com.google.gson.Gson;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.reflect.TypeToken;
+import java.net.URI;
+import org.apache.beam.sdk.extensions.gcp.util.gcsfs.GcsPath;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class DLPTemplateHelper {
+  public static final Logger LOG = LoggerFactory.getLogger(DLPTemplateHelper.class);
+  public static final String DLP_DEID_CONFIG_FILE = "de-identify-config.config";
+  public static final Gson gson = new Gson();
+
+  public static void main(String[] args) {
+    String deidDlpConfiig = getDeidConfigJson();
+    String gcsBucket = args[0];
+    JsonObject kekConfig =
+        gson.fromJson(getKekDetails(gcsBucket), new TypeToken<JsonObject>() {}.getType());
+    JsonElement kek = kekConfig.get("ciphertext");
+    // take out version -DLP template does not allow
+    String keyName= gson.toJson((kekConfig.get("name").getAsString().split("/cryptoKeyVersions/")[0]));
+    
+    String updatedDeidConfig =
+        String.format(
+            deidDlpConfiig,
+            keyName,
+            kek,
+            keyName,
+            kek);
+    LOG.info(
+        "Successfully Updated DLP De-Identification Configuration With KEK {}",
+        uploadConfig(updatedDeidConfig.trim(), gcsBucket));
+  }
+
+  public static String getKekDetails(String gcsPath) {
+    GcsPath path = GcsPath.fromUri(URI.create(gcsPath));
+    Storage storage = StorageOptions.getDefaultInstance().getService();
+    BlobId blobId = BlobId.of(path.getBucket(), path.getObject());
+    byte[] content = storage.readAllBytes(blobId);
+    String contentString = new String(content, UTF_8);
+    LOG.debug("KEK {}", contentString);
+    return contentString;
+  }
+
+  public static BlobId uploadConfig(String contents, String gcsPath) {
+    GcsPath path = GcsPath.fromUri(URI.create(gcsPath));
+    Storage storage = StorageOptions.getDefaultInstance().getService();
+    BlobId blobId = BlobId.of(path.getBucket(), "de-identify-config.json");
+    BlobInfo blobInfo = BlobInfo.newBuilder(blobId).setContentType("application/json").build();
+    Blob blob = storage.create(blobInfo, contents.getBytes(UTF_8));
+    return blob.getBlobId();
+  }
+
+  public static String getDeidConfigJson() {
+    String schemaJson = null;
+    try {
+      schemaJson = Resources.toString(Resources.getResource(DLP_DEID_CONFIG_FILE), UTF_8);
+    } catch (Exception e) {
+      LOG.error("Unable to read {} file from the resources folder!", DLP_DEID_CONFIG_FILE, e);
+    }
+
+    return schemaJson;
+  }
+}
