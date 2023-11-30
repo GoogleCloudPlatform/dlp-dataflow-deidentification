5d17da59e8724ddb7a92bc58b0f7dd2529e37c85
add-apt-repository <143883428+add-apt-repository@users.noreply.github.com>
2023-11-30 20:41:27 +0530

    shell format

--- create-kek.sh
+++ create-kek.sh
@@ -13,7 +13,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.#!/usr/bin/env bash
 
-set -x 
+set -x
 PROJECT_ID=$1
 KEY_RING_NAME=$2
 KEY_NAME=$3
@@ -23,9 +23,7 @@ API_KEY=$6
 API_ROOT_URL="https://cloudkms.googleapis.com"
 KEK_API="${API_ROOT_URL}/v1/projects/${PROJECT_ID}/locations/global/keyRings/${KEY_RING_NAME}/cryptoKeys/${KEY_NAME}:encrypt"
 curl -X POST -H "Content-Type: application/json" \
- -H "Authorization: Bearer ${API_KEY}" \
- "${KEK_API}"`` \
- -d '{"plaintext":"'${TEK}'"}' \
- -o "${KEK}"	 
-
-
+    -H "Authorization: Bearer ${API_KEY}" \
+    "${KEK_API}"$() \
+    -d '{"plaintext":"'${TEK}'"}' \
+    -o "${KEK}"
